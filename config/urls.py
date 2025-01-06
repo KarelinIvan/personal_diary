e8f354e33cd1ec96a1fbfaca37b74af9857d4629
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-01-06 19:08:54 +0300

    59-й коммит,

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('users/', include('users.urls', namespace='users')),
     path('diary/', include('diary.urls', namespace='diary')),
     path("ckeditor/", include('ckeditor_uploader.urls')),
